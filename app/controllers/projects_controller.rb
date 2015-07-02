c168101d408bc2f11484e8a8fe926404b08d26d1
DavidAbel <drabelpdx@gmail.com>
2015-07-02 12:15:12 -0700

    Adds GitHub link to project.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -44,7 +44,7 @@ class ProjectsController < ApplicationController
 
   private
   def project_params
-    params.require(:project).permit(:name, :description)
+    params.require(:project).permit(:name, :description, :link)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,24 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150702171932) do
+ActiveRecord::Schema.define(version: 20150702182509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
-    t.string   "description"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
-    t.string   "description"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "category_id"
+    t.string   "link"
   end
 
 end
