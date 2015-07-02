92990f675a8498505d7773e9e9cf796669749215
DavidAbel <drabelpdx@gmail.com>
2015-07-02 07:19:58 -0700

    Validates projects.

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150702130202) do
+ActiveRecord::Schema.define(version: 20150702141812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "projects", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
     t.datetime "created_at"
